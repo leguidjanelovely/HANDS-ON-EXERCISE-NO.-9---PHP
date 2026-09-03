95749c0bfbf37a149ff9fe874798e32b5d8667c5
leguidjanelovely <leguidjanelovely@gmail.com>
2026-09-03 20:28:09 +0800

    Add index.php for website homepage

---
+++ #2 index.php
@@ -0,0 +1,17 @@
+<?php include 'header.php'; ?>
+
+<div class="card">
+    <h1>Welcome to My Website</h1>
+
+    <p>
+        Welcome! This is my PHP Output #2 project.
+        You can register an account, login, or recover your password.
+    </p>
+
+    <a href="register.php" class="button">Get Started</a>
+</div>
+
+</div>
+
+</body>
+</html>

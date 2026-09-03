914742a671b7897c92f2fb6498ae5a7115af77b2
leguidjanelovely <leguidjanelovely@gmail.com>
2026-09-03 20:30:38 +0800

    Create #2 login.php

---
+++ #2 login.php
@@ -0,0 +1,31 @@
+<?php include 'header.php'; ?>
+
+<div class="card">
+    <h2>Login</h2>
+
+    <form action="" method="POST">
+
+        <label>Email</label>
+        <input type="email" name="email" required>
+
+        <label>Password</label>
+        <input type="password" name="password" required>
+
+        <button type="submit">Login</button>
+
+    </form>
+
+    <p>
+        <a href="forgot_password.php">Forgot Password?</a>
+    </p>
+
+    <p>
+        Don't have an account?
+        <a href="register.php">Register here</a>
+    </p>
+</div>
+
+</div>
+
+</body>
+</html>

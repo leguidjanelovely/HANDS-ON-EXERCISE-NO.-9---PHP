1b962b625223769a0f36b18af4df0a86c90f9ebd
leguidjanelovely <leguidjanelovely@gmail.com>
2026-09-03 20:09:40 +0800

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,17 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <h2>Welcome to My Website!</h2>
+
+    <p>
+        Welcome! This is the Home Page of my PHP Output #2.
+    </p>
+
+    <p>
+        Please use the navigation menu above to Register or Login.
+    </p>
+</div>
+
+</main>
+</body>
+</html>

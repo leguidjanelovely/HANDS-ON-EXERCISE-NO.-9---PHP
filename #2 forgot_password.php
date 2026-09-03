d4fb9bc8c3d97df562ac9f53e610ac1aa5484071
leguidjanelovely <leguidjanelovely@gmail.com>
2026-09-03 20:32:07 +0800

    Add forgot password page

---
+++ #2 forgot_password.php
@@ -0,0 +1,26 @@
+<?php include 'header.php'; ?>
+
+<div class="card">
+    <h2>Forgot Password</h2>
+
+    <p>Enter your email address to reset your password.</p>
+
+    <form action="" method="POST">
+
+        <label>Email Address</label>
+        <input type="email" name="email" required>
+
+        <button type="submit">Reset Password</button>
+
+    </form>
+
+    <p>
+        Remember your password?
+        <a href="login.php">Login here</a>
+    </p>
+</div>
+
+</div>
+
+</body>
+</html>
